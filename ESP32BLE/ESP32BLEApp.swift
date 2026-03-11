79cd378c2bf5326ddaec190a4abf9d882ecc9aee
Rick <github@mindtn.com>
2026-03-11 12:57:47 +0900

    Initial Commit

---
+++ ESP32BLE/ESP32BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESP32BLEApp.swift
+//  ESP32BLE
+//
+//  Created by Eric Beresford on 2026/03/11.
+//
+
+import SwiftUI
+
+@main
+struct ESP32BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
