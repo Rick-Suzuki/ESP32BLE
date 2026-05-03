1237ef51ad857fbf3cfe04aa3ddc8f683f4177cb
Rick-Suzuki <92418508+Rick-Suzuki@users.noreply.github.com>
2026-05-03 15:40:24 +0900

    test commit after renaming folders

--- ESP32BLE/Scrns/MainScreen/MainScreen.swift
+++ ESP32BLE/Scrns/MainScreen/MainScreen.swift
@@ -3,6 +3,7 @@ import SwiftUI
 import UIKit
 import AVFoundation
 
+
 struct MainScreen: View {
     // Easy-to-find styling controls for the main button grid.
     private let mainGridButtonSpacingMaximum: CGFloat = 10
